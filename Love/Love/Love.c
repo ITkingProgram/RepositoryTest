ea137e7782ce7dd6086aa8c093e8787c05b30d5f
ITkingProgram <2056603086@qq.com>
2023-04-17 12:29:28 +0800

    爱心代码

---
+++ Love/Love/Love.c
@@ -0,0 +1,20 @@
+#define _CRT_SECURE_NO_WARNINGS 1 
+#include<stdio.h>
+#include<Windows.h>
+
+void main() {
+	float x, y, a;
+	for (y = 1.5; y > -1.5; y -= 0.1) {
+		for (x = -1.5; x < 1.5; x += 0.05)
+		{
+			a = x * x + y * y - 1;
+			putchar(a * a * a - x * x * y * y * y <= 0.0 ? '*' : ' ');
+		}
+		system("color 71");
+		putchar('\n');
+	}
+	printf("WZM同学\n");
+	puts("");
+	printf("记得自强不息噢！！！");
+	puts("");
+}
